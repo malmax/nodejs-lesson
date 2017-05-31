051c75ea77cef25596d79772f3baca89b2c5b3eb
malmax <malmax.spb@gmail.com>
2017-05-31 13:56:46 +0300

    complete task3

--- task3/src/index.js
+++ task3/src/index.js
@@ -1,6 +1,23 @@
 import beep from 'beepbeep';
+import chalk from 'chalk';
 
-beep([1000, 500, 2000, 500, 1000]);
+const argv = process.argv[2] || 'normal';
 
-let text = "World";
-console.log(`Hello ${text.toUpperCase()} \u0007`);
+switch (argv) {
+  case 'inverse': {
+    console.log(chalk.black.bgCyan.bold('Hello, World!'));
+    beep([1000, 500]);
+    console.log(chalk.underline.white.bgBlack('this is my inverse message =)'));
+    beep([1000]);
+
+    break;
+  }
+  default: {
+    console.log(chalk.white.bgBlue.bold('Hello, World!'));
+    beep([1000, 500]);
+    console.log(chalk.underline.black.bgWhite('this is my message =)'));
+    beep([1000, 500, 2000]);
+
+    break;
+  }
+}
