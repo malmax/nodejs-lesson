ac840cee78eb0f98e16604d0b133f58668e92287
malmax.spb@gmail.comm <malmax.spb@gmail.com>
2017-06-07 23:15:21 +0300

    complete task1

--- task1/src/index.js
+++ task1/src/index.js
@@ -1,2 +1,15 @@
-let text = "World";
-console.log(`Hello ${text.toUpperCase()}`);
+import request from 'request';
+import cheerio from 'cheerio';
+
+request('http://www.newsru.com/', (error, response, html) => {
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(html);
+    $('.right-feed-item').each((i, element) => {
+      const time = $(element).find('span').text().trim();
+      const link = $(element).find('a');
+      if (!time || !link.attr('title')) { return; }
+
+      console.log(time, link.attr('title'));
+    });
+  }
+});
